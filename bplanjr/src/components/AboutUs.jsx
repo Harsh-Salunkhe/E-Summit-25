a8bbdf1dc6d9417bf1f892769164e7ca895b96ca
Harsh-Salunkhe <harshsalunkhe120526@gmail.com>
2025-09-20 04:28:24 +0530

    Revert "Update AboutUs.jsx"
    This reverts commit 2f2722bfacda8cce81a09e9c0e8101433fc2630c.

--- bplanjr/src/components/AboutUs.jsx
+++ bplanjr/src/components/AboutUs.jsx
@@ -46,7 +46,7 @@ const AboutUs = () => (
           </p>
           <div className="space-y-4">
             {[
-              "Prizes and Goodies worth INR 20,000",
+              "Prize pool of ₹1 Lakh+",
               "Mentorship from industry leaders",
               "Exclusive networking opportunities",
               "Direct access to investors",
